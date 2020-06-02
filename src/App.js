f28fbf0fe55effbfef2c563ef39f33a3318795d7
KataV <KataV@users.noreply.github.com>
2020-06-02 12:44:38 +0300

    Styling and added functionality to the search button

--- src/App.js
+++ src/App.js
@@ -3,89 +3,148 @@ import { authEndpoint, clientId, redirectUri, scopes } from "./Config";
 import $ from "jquery";
 import hash from "./hash";
 import Player from "./Player";
-import './App.css';
+import "./App.css";
 
 const App = () => {
+  const [_token, setToken] = useState(undefined);
+  const [tracks, setTracks] = useState([]);
+  const [artist, setArtist] = useState([]);
+  const [textLine, setTextLine] = useState(
+    "Please select a year and click the button!"
+  );
 
-    const [_token, setToken] = useState(undefined)
-    const [tracks, setTracks] = useState([])
-    const [artist, setArtist] = useState([])
+  useEffect(() => {
+    /*  const token = undefined */
+    let _token = hash.access_token;
 
+    if (_token) {
+      setToken(_token);
+    }
+    $.ajax({
+      url:
+        "https://api.spotify.com/v1/search?q=year%3A2019&type=track&market=US&limit=10",
+      type: "GET",
+      beforeSend: (xhr) => {
+        xhr.setRequestHeader("Authorization", "Bearer " + _token);
+      },
+      success: (data) => {
+        const song = data.tracks.items.map((i) => {
+          console.log(i.name);
+          return i.name;
+        });
 
-    useEffect(() => {
-        /*  const token = undefined */
-        let _token = hash.access_token;
+        setTracks(song);
+        /* console.log(song) */
 
-        if (_token) {
-            setToken(_token)
-        }
-        $.ajax({
-            url: "https://api.spotify.com/v1/search?q=year%3A1985&type=track&market=US&limit=10",
-            type: "GET",
-            beforeSend: xhr => {
-                xhr.setRequestHeader("Authorization", "Bearer " + _token);
-            },
-            success: data => {
-                const song = data.tracks.items.map((i) => {
-                    console.log(i.name)
-                    return i.name
-                });
+        const artist = data.tracks.items.map((i) => {
+          console.log(i.artists);
+          return i.artists.map((b) => {
+            console.log(b.name);
+            return b.name;
+          });
+        });
+        setArtist(artist);
 
-                setTracks(song)
-                /* console.log(song) */
+        console.log(artist);
+        const trackList = data.tracks.items.map((i, index) => (
+          <span key={index}>{i.name}</span>
+        ));
 
-                const artist = data.tracks.items.map((i) => {
-                    console.log(i.artists)
-                    return i.artists.map((b) => {
-                        console.log(b.name)
-                        return b.name
+        setTracks(trackList);
 
-                    })
-                })
-                setArtist(artist)
-
-                console.log(artist)
-                const trackList = data.tracks.items.map((i, index) =>
-                    (<span key={index}>{i.name}</span>))
-
-                setTracks(trackList)
-
-                const artistList = data.tracks.items.map((i) =>
-                    i.artists.map((b, index) =>
-                        (<span key={index}>{b.name} </span>))
-                )
-                setArtist(artistList)
-                /*  item: data.item,
+        const artistList = data.tracks.items.map((i) =>
+          i.artists.map((b, index) => <span key={index}>{b.name} </span>)
+        );
+        setArtist(artistList);
+        /*  item: data.item,
                  is_playing: data.is_playing,
                  progress_ms: data.progress_ms */
+      },
+    });
+  }, []);
 
-            }
-        });
-    }, [])
-
-    return (
-        <div>
-
-            {!_token && (
-                <a
-                    className="btn btn--loginApp-link"
-                    href={`${authEndpoint}?client_id=${clientId}&redirect_uri=${redirectUri}&scope=${scopes.join(
-                        "%20"
-                    )}&response_type=token&show_dialog=true`}
-                >
-                    Login to Spotify
-                </a>
-            )}
-            {_token && (
-                <Player
-                    tracks={tracks}
-                    artist={artist} />
-            )}
-
-        </div>
+  const searchYear = () => {
+    console.log("This was clicked");
+    let searchedYear = document.getElementById("year").value;
+    console.log(searchedYear);
+    setTextLine(
+      "Here is your playlist for the year " + searchedYear + ", enjoy!"
     );
-}
+    $.ajax({
+        url: "https://api.spotify.com/v1/search?q=year%3A" + searchedYear + "&type=track&market=US&limit=10",
+        headers: {
+            'Authorization': 'Bearer ' + _token
+        },
+        success: (data) => {
+            const song = data.tracks.items.map((i) => {
+              console.log(i.name);
+              return i.name;
+            });
+    
+            setTracks(song);
+            /* console.log(song) */
+    
+            const artist = data.tracks.items.map((i) => {
+              console.log(i.artists);
+              return i.artists.map((b) => {
+                console.log(b.name);
+                return b.name;
+              });
+            });
+            setArtist(artist);
+    
+            console.log(artist);
+            const trackList = data.tracks.items.map((i, index) => (
+              <span key={index}>{i.name}</span>
+            ));
+    
+            setTracks(trackList);
+    
+            const artistList = data.tracks.items.map((i) =>
+              i.artists.map((b, index) => <span key={index}>{b.name} </span>)
+            );
+            setArtist(artistList);
+            /*  item: data.item,
+                     is_playing: data.is_playing,
+                     progress_ms: data.progress_ms */
+          },
+        });
+   
+  };
+
+  return (
+    <div className="App">
+      <h1>Music From My Year</h1>
+      {!_token && (
+        <a
+          className="btn btn--loginApp-link"
+          href={`${authEndpoint}?client_id=${clientId}&redirect_uri=${redirectUri}&scope=${scopes.join(
+            "%20"
+          )}&response_type=token&show_dialog=true`}
+        >
+          Login to Spotify
+        </a>
+      )}
+      {_token && (
+        <div>
+          <div className="searchContainer">
+            <h3>Search bar thingy stuff here</h3>
+            <div>
+              <input type="number" id="year" defaultValue="1990"></input>
+              <button className="searchButton" onClick={searchYear}>
+                Get your playlist!
+              </button>
+            </div>
+            <div className="enjoyBanner">
+              <p>{textLine}</p>
+            </div>
+          </div>
+          <Player tracks={tracks} artist={artist} />
+        </div>
+      )}
+    </div>
+  );
+};
 
 export default App;
-
 
