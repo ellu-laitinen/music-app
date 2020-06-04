e08be68e643461143dad8dfcf8dd189ca2e04c63
KataV <KataV@users.noreply.github.com>
2020-06-04 11:30:13 +0300

    Testing some styling

--- src/App.js
+++ src/App.js
@@ -6,165 +6,136 @@ import Player from "./Player";
 import "./App.css";
 
 const App = () => {
-    const [_token, setToken] = useState(undefined);
-    const [tracks, setTracks] = useState();
-    const [artist, setArtist] = useState();
-    const [textLine, setTextLine] = useState(
-        "Please select a year and click the button!"
+  const [_token, setToken] = useState(undefined);
+  const [tracks, setTracks] = useState();
+  const [artist, setArtist] = useState();
+  const [image, setImage] = useState();
+  const [textLine, setTextLine] = useState(
+    "Please select a year and click the button!"
+  );
+ 
+
+  useEffect(() => {
+    let _token = hash.access_token;
+
+    if (_token) {
+      setToken(_token);
+    }
+  }, []);
+
+  const searchYear = () => {
+    console.log("This was clicked");
+    let searchedYear = document.getElementById("year").value;
+    console.log(searchedYear);
+    setTextLine(
+      "Here is your playlist for the year " + searchedYear + ", enjoy!"
     );
-
-    useEffect(() => {
-        /*  const token = undefined */
-        let _token = hash.access_token;
-
-        if (_token) {
-            setToken(_token);
-        }
-        /*  $.ajax({
-             url:
-                 "https://api.spotify.com/",
-             type: "GET",
-             beforeSend: (xhr) => {
-                 xhr.setRequestHeader("Authorization", "Bearer " + _token);
-             }, *//* 
-success: (data) => { */
-        /*   const song = data.tracks.items.map((i) => {
-              console.log(i.name);
-              return i.name;
-          }); */
-
-        /* setTracks(song); */
-        /* console.log(song) */
-
-        /*  const artist = data.tracks.items.map((i) => {
-             console.log(i.artists);
-             return i.artists.map((b) => {
-                 console.log(b.name);
-                 return b.name;
-             });
-         }); */
-        /* setArtist(artist); */
-        /* 
-                        console.log(artist);
-                        const trackList = data.tracks.items.map((i, index) => (
-                            <span key={index}>{i.name}</span>
-                        ));
-        
-                        setTracks(trackList); */
-
-        /*    const artistList = data.tracks.items.map((i) =>
-               i.artists.map((b, index) => <span key={index}>{b.name} </span>)
-           );
-           setArtist(artistList); */
-        /*  item: data.item,
-                 is_playing: data.is_playing,
-                 progress_ms: data.progress_ms */
-        /*  } *//* , */
-        /* }); */
-    }, []);
-
-    const searchYear = () => {
-        console.log("This was clicked");
-        let searchedYear = document.getElementById("year").value;
-        console.log(searchedYear);
-        setTextLine(
-            "Here is your playlist for the year " + searchedYear + ", enjoy!"
-        );
-        $.ajax({
-            url: "https://api.spotify.com/v1/search?q=year%3A" + searchedYear + "&type=track&market=US&limit=10",
-            headers: {
-                'Authorization': 'Bearer ' + _token
-            },
-            success: (data) => {
-                const song = data.tracks.items.map((i) => {
-                    /* console.log(i.name); */
-                    return i.name;
-                });
-
-                setTracks(song);
-                /* console.log(song) */
-
-                const artist = data.tracks.items.map((i) => {
-                    /* console.log(data.tracks.items) */
-                    /* if (i.artists.length > 1) { */
-                    /* console.log(i.artists) */
-                    /*   i.artists.map((c) => { */
-                    /* console.log(c.name) */
-                    /*    return c.name
-                   })
-*/
-                    /* } */
-                    /*  console.log(i.artists); */
-                    return i.artists.map((b) => {
-                        console.log(b.name);
-                        return b.name;
-                    });
-                });
-                setArtist(artist);
-
-
-                console.log(artist);
-                const trackList = data.tracks.items.map((i, index) => (
-                    <p key={i.id} >{i.name}</p>
-                ));
-
-                setTracks(trackList);
-
-                const artistList = data.tracks.items.map((i) => (
-                    <p key={i.id}>{i.artists.map((b) => {
-                        console.log(b.name)
-                        if (i.artists.length > 1) {
-                            return b.name + (', ')
-                        }
-                        return b.name + (' ')
-                    })} </p>)
-
-                );
-
-                artistList.toString()
-                setArtist(artistList);
-                /*  item: data.item,
-                         is_playing: data.is_playing,
-                         progress_ms: data.progress_ms */
-            },
+    $.ajax({
+      url:
+        "https://api.spotify.com/v1/search?q=year%3A" +
+        searchedYear +
+        "&type=track&market=US&limit=10",
+      headers: {
+        Authorization: "Bearer " + _token,
+      },
+      success: (data) => {
+        const song = data.tracks.items.map((i) => {
+          /* console.log(i.name); */
+          return i.name;
         });
 
-    };
+        setTracks(song);
+        /* console.log(song) */
 
-    return (
-        <div className="App">
-            <h1>Music From My Year</h1>
-            {!_token && (
-                <a
-                    className="btn btn--loginApp-link"
-                    href={`${authEndpoint}?client_id=${clientId}&redirect_uri=${redirectUri}&scope=${scopes.join(
-                        "%20"
-                    )}&response_type=token&show_dialog=true`}
-                >
-                    Login to Spotify
-                </a>
-            )}
-            {_token && (
-                <div>
-                    <div className="searchContainer">
-                        <h3>Search bar thingy stuff here</h3>
-                        <div>
-                            <input type="number" id="year" defaultValue="1990"></input>
-                            <button className="searchButton" onClick={searchYear}>
-                                Get your playlist!
+        console.log(data);
+
+        const artist = data.tracks.items.map((i) => {
+          return i.artists.map((b) => {
+            console.log(b.name);
+            return b.name;
+          });
+        });
+        setArtist(artist);
+        console.log(artist);
+
+        const trackList = data.tracks.items.map((i, index) => (
+          <p key={i.id}>{i.name}</p>
+        ));
+        setTracks(trackList);
+
+        const artistList = data.tracks.items.map((i) => (
+          <p key={i.id}>
+            {i.artists.map((b) => {
+              console.log(b.name);
+              if (i.artists.length > 1) {
+                return b.name + ", ";
+              }
+              return b.name + " ";
+            })}{" "}
+          </p>
+        ));
+
+        artistList.toString();
+        setArtist(artistList);
+
+        const image = data.tracks.items.map((i) => {
+          console.log(i.album.images[1].url)
+          return i.album.images[1].url
+      })
+      /*   
+      setImage(image) */
+
+
+
+      const imageList = data.tracks.items.map((i) => (
+          <img key={i.id} src={i.album.images[1].url} alt="" />
+
+
+      ))
+
+      setImage(imageList)
+      console.log(image)
+
+      },
+
+
+
+    });
+  };
+
+  return (
+    <div className="App">
+      <h1>Music From My Year</h1>
+      {!_token && (
+        <a
+          className="btn btn--loginApp-link"
+          href={`${authEndpoint}?client_id=${clientId}&redirect_uri=${redirectUri}&scope=${scopes.join(
+            "%20"
+          )}&response_type=token&show_dialog=true`}
+        >
+          Login to Spotify
+        </a>
+      )}
+      {_token && (
+        <div>
+          <div className="searchContainer">
+            <h3>Search bar thingy stuff here</h3>
+            <div>
+              <input type="number" id="year" defaultValue="1990"></input>
+              <button className="searchButton" onClick={searchYear}>
+                Get your playlist!
               </button>
-                        </div>
-                        <div className="enjoyBanner">
-                            <p>{textLine}</p>
-                            <Player tracks={tracks} artist={artist} />
-                        </div>
-                    </div>
-
-                </div>
-            )}
+            </div>
+            <div className="enjoyBanner">
+              <p>{textLine}</p>
+              
+              <Player tracks={tracks} artist={artist} image={image}/>    
+              </div>       
+          </div>
         </div>
-    );
+      )}
+    </div>
+  );
 };
 
 export default App;
-
--- src/Player.js
+++ src/Player.js
@@ -1,7 +1,9 @@
 import React from "react";
 import "./Player.css";
 
-const Player = ({ tracks, artist }) => {
+const Player = ({ tracks, artist, image }) => {
+
+  
 
   if (!tracks && !artist) {
     return (
@@ -9,15 +11,32 @@ const Player = ({ tracks, artist }) => {
     )
   }
 
+  
+  
+
   return (
-    <tbody className="App">
-      <tr className="songs">
-        <td className="songlist" colspan="2">List of songs: {tracks}</td>
-        <th className="artistlist">Artists: {artist}</th>
 
-      </tr>
+    <div className="App">
+      <div className="songs">
+        <div className="songlist">List of songs: {tracks}</div>
+        <div className="artistlist">Artists: {artist}</div>
+        <div className="imagelist"> List of images: {image}
+          
 
-    </tbody>
+        </div>
+      </div>
+
+    </div>
+//     <table>
+// <tbody className="App">
+//       <tr className="songs">
+//         <td className="songlist" >List of songs: {tracks}</td>
+//         <td className="artistlist" >Artists: {artist}</td>
+
+//       </tr>
+
+//     </tbody>
+//     </table>
   );
 }
 
