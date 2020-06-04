c7bb0eae73a83909a4422971615a0c4b5ecf55a2
ElluLaitinen <ellu.laitinen85@gmail.com>
2020-06-04 13:41:40 +0300

    add search options

--- src/App.js
+++ src/App.js
@@ -10,66 +10,46 @@ const App = () => {
     const [tracks, setTracks] = useState();
     const [artist, setArtist] = useState();
     const [image, setImage] = useState();
-    const [link, setLink] = useState();
+    const link = undefined
     const [textLine, setTextLine] = useState(
         "Please select a year and click the button!"
     );
 
     useEffect(() => {
-
         let _token = hash.access_token;
-
         if (_token) {
             setToken(_token);
         }
-
     }, []);
 
     const searchYear = () => {
         console.log("This was clicked");
         let searchedYear = document.getElementById("year").value;
         console.log(searchedYear);
+        let chosenLimit = document.getElementById("limit").value;
+        console.log(chosenLimit);
+        let chosenGenre = document.getElementById("genre").value;
+        console.log(chosenGenre);
+        let chosenMarket = document.getElementById("market").value;
+        console.log(chosenMarket);
         setTextLine(
-            "Here is your playlist for the year " + searchedYear + ", enjoy!"
+            "Here are your " + chosenLimit + " " + chosenGenre + " songs from the year " + searchedYear + ", enjoy!"
         );
         $.ajax({
-            url: "https://api.spotify.com/v1/search?q=year%3A" + searchedYear + "&type=track&market=US&limit=10",
+            url: "https://api.spotify.com/v1/search?q=year%3A" + searchedYear + "%20genre:" + chosenGenre + "&type=track&market=" + chosenMarket + "&limit=" + chosenLimit,
             headers: {
                 'Authorization': 'Bearer ' + _token
             },
             success: (data) => {
-                /*   const song = data.tracks.items.map((i) => {
-                      return i.name;
-                  });
-  
-                  setTracks(song); */
 
-                /* const artist = data.tracks.items.map((i) => { */
                 console.log(data.tracks.items)
-                /*     return i.artists.map((b) => {
-                        return b.name;
-                    });
-                }); */
-
-                /* const link = data.tracks.items.map((i) => {
-                    console.log(i.external_urls.spotify)
-                    return i.external_urls.spotify
-                })
-                setLink(link) */
-
 
                 const trackList = data.tracks.items.map((i) =>
                     <p className="tooltip" key={i.id} ><a target="_blank" rel="noopener noreferrer" href={i.external_urls.spotify}>{i.name}<span className="tooltipText">Listen in Spotify</span></a></p>
                 );
 
                 setTracks(trackList);
-                /* console.log(song) */
 
-                /*  const linkList = data.tracks.items.map((i) =>
-                     <a key={i.id} href={i.external_urls.spotify}>Listen in Spotify</a>
-                 ) */
-                /* 
-                                setLink(linkList) */
 
                 const artistList = data.tracks.items.map((i) => (
                     <p key={i.id}>{i.artists.map((b) => {
@@ -83,31 +63,20 @@ const App = () => {
                 );
 
                 setArtist(artistList);
-                /*     console.log(artist) */
-
-
 
                 const image = data.tracks.items.map((i) => {
-                    /* console.log(i.album.images[1].url) */
+
                     return i.album.images[2].url
                 })
-                /*   
-                setImage(image) */
-
-
-
 
                 const imageList = data.tracks.items.map((i) => (
                     <img key={i.id} src={i.album.images[2].url} alt="" />
-
-
                 ))
 
                 setImage(imageList)
                 console.log(image)
             },
         });
-
     };
 
     return (
@@ -128,7 +97,51 @@ const App = () => {
                     <div className="searchContainer">
                         <h3>Search bar thingy stuff here</h3>
                         <div>
-                            <input type="number" id="year" defaultValue="1990"></input>
+                            <p>Select year <input type="number" id="year" defaultValue="1990" min="1900" max="2030"></input></p>
+                            <p>Select genre <select id="genre" defaultValue="pop">
+                                <option value="ambient">Ambient</option>
+                                <option value="black-metal">Black-metal</option>
+                                <option value="blues">Blues</option>
+                                <option value="classical">Classical</option>
+                                <option value="country">Country</option>
+                                <option value="death-metal">Death-metal</option>
+                                <option value="disco">Disco</option>
+                                <option value="drum-and-bass">Drum-and-bass</option>
+                                <option value="electronic">Electronic</option>
+                                <option value="folk">Folk</option>
+                                <option value="funk">Funk</option>
+                                <option value="gospel">Gospel</option>
+                                <option value="grunge">Grunge</option>
+                                <option value="hard-rock">Hard-rock</option>
+                                <option value="hip-hop">Hip-hop</option>
+                                <option value="house">House</option>
+                                <option value="industrial">Industrial</option>
+                                <option value="jazz">Jazz</option>
+                                <option value="latin">Latin</option>
+                                <option value="metal">Metal</option>
+                                <option value="new-age">New-age</option>
+                                <option value="opera">Opera</option>
+                                <option value="pop">Pop</option>
+                                <option value="punk">Punk</option>
+                                <option value="reggae">Reggae</option>
+                                <option value="rock">Rock</option>
+                                <option value="salsa">Salsa</option>
+                                <option value="samba">Samba</option>
+                                <option value="soul">Soul</option>
+                                <option value="tango">Tango</option>
+                                <option value="techno">Techno</option>
+                            </select></p>
+                            <p>Select market <select id="market">
+                                <option value="US">United States</option>
+                                <option value="FI">Finland</option>
+                                <option value="SE">Sweden</option>
+                                <option value="EE">Estonia</option>
+                                <option value="DE">Germany</option>
+                                <option value="FR">France</option>
+                                <option value="ES">Spain</option>
+                                <option value="GB">Great Britain</option>
+                            </select></p>
+                            <p>Number of songs <input type="number" id="limit" defaultValue="10" min="5" max="50"></input></p>
                             <button className="searchButton" onClick={searchYear}>
                                 Get your playlist!
               </button>
