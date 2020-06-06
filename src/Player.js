980f0235a7091369eac1d31e28f1734461bc15aa
ElluLaitinen <ellu.laitinen85@gmail.com>
2020-06-06 14:19:04 +0300

    add conditional rendering for songs that don't have preview url

--- src/Player.js
+++ src/Player.js
@@ -15,10 +15,15 @@ const Player = ({ trackList, artist, image, link, songLink }) => {
       <img src={image} alt="album cover" className="imagelist" />
       <p><a target="_blank" rel="noopener noreferrer" href={link}>Listen in Spotify</a> </p>
       <div>
-        <p>30 sec sample:</p>
-        <iframe src={songLink} title="Spotify play button" width="300" height="80" frameborder="0" allowtransparency="true" allow="encrypted-media"></iframe>
+        <p>30 sec sample:</p> <div>
+          {songLink === null ? (
+            < div> No sample available for this song :( </div>
+          ) :
+            < iframe src={songLink} title="Spotify play button" width="300" height="80" frameborder="0" allowtransparency="true" allow="encrypted-media"></iframe>
+          }
+        </div>
       </div>
-    </div>
+    </div >
   );
 }
 
