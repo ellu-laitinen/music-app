0c647c08ee799566770ef7573c0823cded10ffc6
lapohjan <59822237+lapohjan@users.noreply.github.com>
2020-06-08 13:35:14 +0300

    Styling of cards

--- src/Player.js
+++ src/Player.js
@@ -1,30 +1,26 @@
 import React from "react";
 import "./Player.css";
 
-
-
 const Player = ({ trackList, artist, image, link, songLink }) => {
 
-
   return (
-
-
     <div className="songs">
+       <img src={image} alt="album cover" className="imagelist" />
       <h2 className="songlist"> {trackList}</h2>
       <h3 className="artistlist"> {artist.join(', ')}</h3>
-      <img src={image} alt="album cover" className="imagelist" />
       <p><a target="_blank" rel="noopener noreferrer" href={link}>Listen in Spotify</a> </p>
       <div>
-        <p>30 sec sample:</p> <div>
-          {songLink === null ? (
-            < div> No sample available for this song :( </div>
-          ) :
-            < iframe src={songLink} title="Spotify play button" width="300" height="80" frameborder="0" allowtransparency="true" allow="encrypted-media"></iframe>
-          }
-        </div>
+        <p>30 sec sample:</p>
+            <div id="songlink">
+                {songLink === null ? (
+                <div> No sample available for this song :( </div>
+                ) :
+                <iframe id="sample" src={songLink} title="Spotify play button" width="300" height="80" frameborder="0" allowtransparency="true" allow="encrypted-media"></iframe>
+                }
+            </div>
       </div>
-    </div >
+    </div>
   );
-}
+} 
 
 export default Player;
